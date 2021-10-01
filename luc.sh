16d69ea51243e63c008dd0882f4ddfb550e68c6d
Morc - Richard Gráčik <r.gracik@gmail.com>
2021-10-01 15:29:44 +0200

    add a few files
    original dataS.gar from Traktor Zetor Simulátor
    gar.bms file from https://aluigi.altervista.org/quickbms.htm used to unpack the dataS file
    luc.sh used to go through all .luc scripts and decompile them to readable .lua

---
+++ luc.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+mkdir dataS_lua
+mkdir dataS_lua/missions
+mkdir dataS_lua/scripts
+mkdir dataS_lua/scripts/environment
+mkdir dataS_lua/scripts/gui
+mkdir dataS_lua/scripts/objects
+mkdir dataS_lua/scripts/sounds
+mkdir dataS_lua/scripts/triggers
+mkdir dataS_lua/scripts/vehicles
+mkdir dataS_lua/scripts/vehicles/specializations
+
+for i in $(find dataS -name '*.luc')
+do
+	echo "original file =" ${i}
+    file_start=${i/dataS/dataS_lua} 
+    file_end=${file_start/%.*/.lua}
+    java -jar unluac_2021_09_23.jar ${i} > ${file_end}
+    echo "end file =" ${file_end}
+    echo "------------------"
+done
+    
+   
