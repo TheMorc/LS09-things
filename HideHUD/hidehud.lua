70243b33b945cabe7c4c0c2e8cea124c413f84cb
Morc - Richard Gráčik <r.gracik@gmail.com>
2021-10-13 23:28:41 +0200

    add HideHUD mod source

---
+++ HideHUD/hidehud.lua
@@ -0,0 +1,71 @@
+-- HideHUD Mod - hidehud.lua
+-- possibly the only file needed for it all
+-- @author  Richard Gráčik (mailto:r.gracik@gmail.com)
+-- @date  13.10.2021
+
+print("[HideHUD Mod] saving original draw and keyEvent functions")
+
+HideHUDenabled=false
+
+original = { 
+	 draw = draw,
+	 keyEvent = keyEvent
+}
+
+--draw function that does exactly nothing
+function HideHUDdraw()
+end
+
+--since gameMenuSystem from main.lua has a higher priority than modClassEventListener's keyEvent then we need a workaround
+function HideHUDkeyEvent(unicode, sym, modifier, isDown)	
+
+	original.keyEvent(unicode, sym, modifier, isDown)
+	
+	if gameMenuSystem.currentMenu ~= nil and HideHUDenabled then
+		_G.draw = original.draw
+		print("[HideHUD Mod] disabling HideHUD - in game menu open")
+		HideHUDenabled = false
+	end
+	
+end
+
+print("[HideHUD Mod] overriding the keyEvent function")
+_G.keyEvent = HideHUDkeyEvent
+
+modClassEventListener = {};
+
+
+function modClassEventListener:loadMap(name)
+
+end;
+
+function modClassEventListener:deleteMap()
+
+end;
+
+function modClassEventListener:mouseEvent(posX, posY, isDown, isUp, button)
+
+end;
+
+function modClassEventListener:keyEvent(unicode, sym, modifier, isDown)
+	if isDown and sym == 291 then
+		HideHUDenabled = not HideHUDenabled
+		
+		if HideHUDenabled then
+			_G.draw = HideHUDdraw
+			print("[HideHUD Mod] enabling HideHUD - F10 key")
+		else
+			_G.draw = original.draw
+			print("[HideHUD Mod] disabling HideHUD - F10 key")
+		end
+	end
+end;
+
+function modClassEventListener:update(dt)
+end;
+
+function modClassEventListener:draw()
+
+end;
+
+addModEventListener(modClassEventListener);
