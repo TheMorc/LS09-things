c1cac9b175497deb3f708446a2332d48c9723713
Richard Gráčik <r.gracik@gmail.com>
2025-01-29 01:50:47 +0100

    guiWidescreenFix v0.2: minor simplification

--- guiWidescreenFix/guiWidescreenFix.lua
+++ guiWidescreenFix/guiWidescreenFix.lua
@@ -13,6 +13,7 @@ local guiWF_scaleRatio = (4/3) / (guiWF_resX / guiWF_resY)
 local guiWF_guiOffset = (1 - guiWF_scaleRatio) / 2
 print("[guiWidescreenFix] New ratio: " .. guiWF_scaleRatio .. " | New GUI offset: " .. guiWF_guiOffset);
 
+--aux calc function
 function guiWF_recalc(x, width)
   local guiWF_x, guiWF_width
   if x == 0 then
@@ -36,34 +37,29 @@ end
 _G.renderText = guiWF_renderText
 print("[guiWidescreenFix] Patched renderText")
 
+guiWF_renderOverlayOriginal = _G.renderOverlay
+function guiWF_renderOverlay(overlayId, x, y, width, height)
+	x, width = guiWF_recalc(x, width)
+	guiWF_renderOverlayOriginal(overlayId, x, y, width, height)
+end
+_G.renderOverlay = guiWF_renderOverlay
+print("[guiWidescreenFix] Patched renderOverlay")
+
 local Overlay_mt = Class(Overlay)
-function Overlay:new(name, overlayFilename, x, y, width, height)
-  if overlayFilename ~= nil then
-    tempOverlayId = createOverlay(name, overlayFilename)
-  end
-
-  local guiWF_x, guiWF_width = guiWF_recalc(x, width)
-  print("[guiWidescreenFix] Patched overlay " .. name)
-  return setmetatable({overlayId = tempOverlayId,x = guiWF_x,y = y,width = guiWF_width,height = height,visible = true,r = 1,g = 1,b = 1,a = 1}, Overlay_mt)	
+function guiWF_checkOverlayOverlap(posX, posY, overlay)
+  guiWF_x, guiWF_width = guiWF_recalc(overlay.x, overlay.width)
+  return posX >= guiWF_x and posX <= guiWF_x + guiWF_width and posY >= overlay.y and posY <= overlay.y + overlay.height
 end
-
-function Overlay:setPosition(x, y)
-  local xT, wT = guiWF_recalc(x, self.width)
-  self.x = xT
-  self.y = y
-end
-function Overlay:setDimension(width, height)
-  local xT, wT = guiWF_recalc(self.x, width)
-  self.width = wT
-  self.height = height
-end
+_G.checkOverlayOverlap = guiWF_checkOverlayOverlap
+print("[guiWidescreenFix] Patched Overlay.checkOverlayOverlap")
 
 modClassEventListener = {};
 
 function modClassEventListener:loadMap(name)
-	g_currentMission.missionStats.pdaMapArrowXPos = g_currentMission.missionStats.pdaMapWidth / 2 - g_currentMission.missionStats.pdaMapArrowSize + guiWF_guiOffset
-	g_currentMission.missionStats.pdaMapPosX = guiWF_guiOffset
-	print("[guiWidescreenFix] Patched PDA")
+	-- deprecated pda patch, not required anymore
+	--g_currentMission.missionStats.pdaMapArrowXPos = g_currentMission.missionStats.pdaMapWidth / 2 - g_currentMission.missionStats.pdaMapArrowSize + guiWF_guiOffset
+	--g_currentMission.missionStats.pdaMapPosX = guiWF_guiOffset
+	--print("[guiWidescreenFix] Patched PDA")
 end;
 
 function modClassEventListener:deleteMap()
